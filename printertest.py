df775fbb686f03e57164d4be40dddae1ed08f6d2
MeaningOf42 <biltimi@gmail.com>
2016-04-01 11:32:29 +0200

    PyPrinterPi version 1.1

--- printertest.py
+++ printertest.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
 from Adafruit_Thermal import *
+import time
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 
 printer.println("Testing...")
 printer.println("If you're seeing this it works")
+time.sleep(0.1)
+printer.println("")
 printer.println("the final version should look something like this: ")
 printer.println("the final version should look something like this: ")
 printer.println("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679")
