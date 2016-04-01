3005e8349be6c3ca63ad6a831e0a781219a825c1
MeaningOf42 <biltimi@gmail.com>
2016-04-01 22:07:33 +0200

    PiPrinterPi version 1.2

---
+++ main.py
@@ -0,0 +1,18 @@
+#import dependancies
+import RPi.GPIO as GPIO
+import time
+import PiPrintLib
+#set up gpio correctly
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23,GPIO.IN)
+
+#set up names for buttons, used for talking to user
+button1_name = "*"
+button2_name = "0"
+button3_name = "#"
+
+#
+PiPrintLib.printer_print("Do you want to cheat and use values from pi_cheat.txt?")
+PiPrintLib.printer_print("If you cheat you won't calculate pi, just print it")
+PiPrintLib.printer_print("If you want to cheat press " + button1_name)
+PiPrintLib.printer_print("If you want to calculate press " + button2_name)
