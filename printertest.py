7270e875f85bc179016f9c1721320115e7251b15
MeaningOf42 <biltimi@gmail.com>
2016-04-01 11:47:47 +0200

    PyPrinterPi version 1.1

--- printertest.py
+++ printertest.py
@@ -8,9 +8,8 @@ printer.setDefault() # Make sure printer is set to default settings.
 
 printer.println("Testing...")
 time.sleep(0.3)
-printer.println("If you're seeing this it works.")
+printer.println("If youre seeing this it works.")
 time.sleep(0.3)
-printer.println("The final version should look something like this: ")
 printer.println("The final version should look something like this: ")
 printer.println("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679")
 printer.println("Here's an empty space made with blank lines:")
