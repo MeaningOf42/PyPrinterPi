e08d5a6160204fe2f2d0030acd2f47f742a2fc62
MeaningOf42 <biltimi@gmail.com>
2016-04-01 22:10:32 +0200

    PiPrinterPi version 1.2

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ button3_name = "#"
 
 #
 PiPrintLib.printer_print("Do you want to cheat and use values from pi_cheat.txt?")
+PiPrintLib.printer_print("")
 time.sleep(0.2)
+PiPrintLib.printer_print("")
 PiPrintLib.printer_print("If you cheat you won't calculate pi, just print it")
 time.sleep(0.2)
+PiPrintLib.printer_print("")
 PiPrintLib.printer_print("If you want to cheat press " + button1_name)
 time.sleep(0.2)
+PiPrintLib.printer_print("")
 PiPrintLib.printer_print("If you want to calculate press " + button2_name)
