0a3c13d297314820d36a40f1d39689f15c3681b9
MeaningOf42 <biltimi@gmail.com>
2016-04-01 11:50:24 +0200

    PyPrinterPi version 1.1

--- printertest.py
+++ printertest.py
@@ -7,6 +7,10 @@ printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 printer.setDefault() # Make sure printer is set to default settings.
 
 printer.println("Testing...")
+time.sleep(0.3)
+printer.println("")
+printer.println("If youre seeing this it works.")
+time.sleep(0.3)
 printer.println("The final version should look something like this: ")
 printer.println("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679")
 printer.println("Here's an empty space made with blank lines:")
